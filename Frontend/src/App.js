6ba37f8a82a21933169fe21fddf7efeea787b7f2
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-06-16 17:35:54 +0530

    first commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          welcome sir, thi line is chenged to check the redeployment...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is a fresh react app without any stuff
     </div>
   );
 }
